9a704cbfbd7a056dee327dde0ba78f27d07d646f
Samuel Davidson <samueldavidson@google.com>
2019-05-10 12:13:29 -0700

    Initial commit. Most of the UI templated. Simple server without business logic.

---
+++ README.md
@@ -0,0 +1,11 @@
+# JWT it down
+
+A webapp toy to learn how JWTs work practically.
+
+To run it enter:
+
+```bash
+node server.js
+```
+
+And navigate to `localhost:3000` in Chrome.
---
+++ server.js
@@ -0,0 +1,50 @@
+/**
+ * @fileoverview Description of this file.
+ */
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const serverIssuer = "JWTItDown"
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname});
+});
+app.get('/styles.css', (req, res) => {
+    res.sendFile('styles.css', {root: __dirname});
+});
+app.get('/client.js', (req, res) => {
+    res.sendFile('client.js', {root: __dirname});
+});
+
+app.get('/newJWT', (req, res) => {
+    console.log(req.body);
+    res.status(200);
+    res.send({
+        "iss": serverIssuer,
+        "sub": "todo",
+        "iat": new Date(),
+    })
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+function encodeJWTToBase64(JOSEHeader, claims) {
+}
+
+function b64EncodeUnicode(str) {
+    // first we use encodeURIComponent to get percent-encoded UTF-8,
+    // then we convert the percent encodings into raw bytes which
+    // can be fed into btoa.
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+        function toSolidBytes(match, p1) {
+            return String.fromCharCode('0x' + p1);
+    }));
+}
+function b64DecodeUnicode(str) {
+    // Going backwards: from bytestream, to percent-encoding, to original string.
+    return decodeURIComponent(atob(str).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
